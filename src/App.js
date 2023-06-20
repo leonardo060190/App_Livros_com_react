6a92b60f86bce0504cf72851dfed01a13bfbb9fa
leonardo060190 <leonardo-morruga@hotmail.com>
2023-06-20 19:15:35 -0300

    foi colocado o BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MenuSuperior from './components/MenuSuperior';
   const App = () => {
   return( // tudo que vai no return é o que aparece na aplicação
   <>
-  <MenuSuperior/>
+  <MenuSuperior />
   </>
   );
 }
