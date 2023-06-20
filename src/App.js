d8245c757e9f994f43403efd2a21a6f253397144
leonardo060190 <leonardo-morruga@hotmail.com>
2023-06-19 21:51:16 -0300

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import MenuSuperior from './components/MenuSuperior';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const App = () => {
+  return( // tudo que vai no return é o que aparece na aplicação
+  <>
+  <MenuSuperior/>
+  </>
   );
 }
+//Serão criados 3 compomentes para essa aplicação
+//MeuSuperior.js
+//InclusaoLivros.js
 
 export default App;
