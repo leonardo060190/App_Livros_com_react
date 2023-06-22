891120496fab68e8f98be58732a8f4c6bb934938
leonardo060190 <leonardo-morruga@hotmail.com>
2023-06-21 21:15:12 -0300

    atualizado o inclusão livros

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import MenuSuperior from './components/MenuSuperior';
+import InclusaoLivros from './components/InclusaoLivros';
 
 
   const App = () => {
   return( // tudo que vai no return é o que aparece na aplicação
   <>
   <MenuSuperior />
-  <inclusaoLivros />
+  <InclusaoLivros />
   </>
   );
 }
--- src/components/InclusaoLivros.js
+++ src/components/InclusaoLivros.js
@@ -4,13 +4,39 @@
 const IncluirLivros = () => {
     return ( //aqui é o que vai ser exibido na tela
         <div className="container">
-         <div className="fst-italic mt-3">Inclusão</div>
-         <form>
-            <div className="form-group">
-                <label htmlFor="titulo">Titulo</label>
-                <input type="text" className="form-control" id="titulo"/>
-            </div>
-         </form>
+            <div className="fst-italic mt-3">Inclusão</div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="titulo">Titulo</label>
+                    <input type="text" className="form-control" id="titulo" required autoFocus />
+                </div>
+                <div className="form-group mt-2">
+                    <label htmlFor="autor">Autor</label>
+                    <input type="text" className="form-control" id="autor" required />
+                </div>
+                <div className="form-group mt-2">
+                    <label htmlFor="foto">URl da foto:</label>
+                    <input type="url" className="form-control" id="foto" required />
+                </div>
+                <div className="row mt-2">
+                    <div className="col-sm-4">
+                        <div className="form-group">
+                            <label htmlFor="ano">Ano de Publicação</label>
+                            <input type="number" className="form-control" id="ano" required />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-8">
+                    <div className="form-group">
+                        <label htmlFor="preco">Preço</label>
+                        <input type="number" className="form-control" id="preco" step={0.01} required />
+                    </div>
+                </div>
+                <input type="submit" className="btn btn-primary mt-3" value="Enviar" />
+                <input type="reset" className="btn btn-danger mt-3 ms-3" value="Limpar" />
+                
+            </form>
+            <div className="alert"></div>
         </div>
 
     );
