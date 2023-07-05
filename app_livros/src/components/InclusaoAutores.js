50def4c7c852c58b159eebacc263e211d5fc2cac
leonardo060190 <leonardo-morruga@hotmail.com>
2023-07-05 15:51:24 -0300

    foi feito  alterações

--- app_livros/src/components/InclusaoAutores.js
+++ app_livros/src/components/InclusaoAutores.js
@@ -9,53 +9,64 @@ const IncluirAutores = () => {
 
     const { register, handleSubmit } = useForm();
     const [aviso, setAviso] = useState("");
+
     //metodo chamado ao enviar form onSubmit
     const salvar = async (campos) => {
         try {
             const resposta = await api.post("autores/cadastro", campos);
+            console.log(resposta)
             setAviso("Autor cadastrado com sucesso!");
         } catch (error) {
             setAviso("Erro ao cadastrar Autor!");
         }
-        //JSON.stringify() converte um objeto javascript para uma string Json
 
+        //limpa os campos do formulário para uma nova inclusão
+        // reset({ nome: "", sobrenome: "", data_nascimento: "", sexo: "", telefone: "", descricao: "", foto:"" });
+        //JSON.stringify() converte um objeto javascript para uma string Json
         //alert(JSON.stringify(campos));
-    }
+    };
+
     //form onSubmit ={handleSubmit(salvar)}
 
     return ( //aqui é o que vai ser exibido na tela
         <div className="container">
-            <div className="fst-italic mt-3">Inclusão de Autores</div>
+            <h4 className="fst-italic mt-3">Inclusão de Autores</h4>
             <form onSubmit={handleSubmit(salvar)}>
-                <div className="form-group">
-                    <label htmlFor="nome">Nome:</label>
-                    <input type="text" className="form-control" id="nome" required autoFocus {...register("nome")} />
-                </div>
-                <div className="form-group mt-2">
-                    <label htmlFor="sobrenome">Sobrenome:</label>
-                    <input type="text" className="form-control" id="autor" required autoFocus {...register("sobrenome")} />
-                </div>
-                <div className="row mt-1">
-                    <div className="col-sm-3">
+                <div className="row mt-2">
+                    <div className="col-sm-6">
                         <div className="form-group">
-                            <label htmlFor="data_nascimento">Data de Nascimento:</label>
-                            <input type="data" className="form-control" id="data_nascimento" required autoFocus {...register("data_nascimento")} />
+                            <label htmlFor="nome">Nome:</label>
+                            <input type="text" className="form-control" id="nome" required autoFocus {...register("nome")} />
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                        <div className="form-group">
+                            <label htmlFor="sobrenome">Sobrenome:</label>
+                            <input type="text" className="form-control" id="autor" required autoFocus {...register("sobrenome")} />
                         </div>
                     </div>
                 </div>
+
                 <div className="row mt-2">
-                    <div className="col-sm-1">
+                    <div className="col-sm-2">
                         <div className="form-group">
                             <label htmlFor="sexo">Sexo:</label>
                             <input type="text" className="form-control" id="sexo" required autoFocus {...register("sexo")} />
                         </div>
                     </div>
-                    <div className="col-sm-8">
+                    <div className="col-sm-4">
+                        <div className="form-group">
+                            <label htmlFor="data_nascimento">Data de Nascimento:</label>
+                            <input type="data" className="form-control" id="data_nascimento" required autoFocus {...register("data_nascimento")} />
+                        </div>
+                    </div>
+                    <div className="col-sm-4">
                         <div className="form-group">
                             <label htmlFor="telefone">Telefone:</label>
                             <input type="text" className="form-control" id="telefone" required autoFocus {...register("telefone")} />
                         </div>
                     </div>
+
                     <div className="form-group mt-2">
                         <div className="mb-3">
                             <label htmlFor="descricao" class="form-label">Descrição:</label>
@@ -63,7 +74,7 @@ const IncluirAutores = () => {
                         </div>
                     </div>
                     <div className="form-group mt-2">
-                        <label htmlFor="foto">URl da foto:</label>
+                        <label htmlFor="foto">URL da Foto:</label>
                         <input type="url" className="form-control" id="foto" required  {...register("foto")} />
                     </div>
 
@@ -72,9 +83,11 @@ const IncluirAutores = () => {
                 <input type="reset" className="btn btn-outline-danger mt-3 ms-3" value="Limpar" />
 
             </form>
-            <div className="alert"></div>
-        </div>
+            <div className={aviso.startsWith("OK!") ? "alert alert-success" :
+                aviso.startsWith("Erro") ? "alert alert-danger" : ""}> {aviso}
+            </div>
 
+        </div>
     );
 
 }
--- app_livros/src/components/InclusaoEditoras.js
+++ app_livros/src/components/InclusaoEditoras.js
@@ -12,57 +12,58 @@ const IncluirEditoras = () => {
     //metodo chamado ao enviar form onSubmit
     const salvar = async (campos) => {
         try {
-            const resposta = await api.post("cadastro/cadastro", campos);
-            setAviso("Editoras cadastrado com sucesso!");
+            const resposta = await api.post("Editoras/cadastro", campos);
+            console.log(resposta)
+            setAviso("Editora cadastrada com sucesso!");
         } catch (error) {
-            setAviso("Erro ao cadastrar Editoras!");
+            console.log(error)
+            setAviso("Erro ao cadastrar Editora!");
         }
-        //JSON.stringify() converte um objeto javascript para uma string Json
 
+
+        //limpa os campos do formulário para uma nova inclusão
+       // reset({ nome: "", sobrenome: "", data_nascimento: "", sexo: "", telefone: "", descricao: "", foto:"" });
+        //JSON.stringify() converte um objeto javascript para uma string Json
         //alert(JSON.stringify(campos));
     }
     //form onSubmit ={handleSubmit(salvar)}
 
     return ( //aqui é o que vai ser exibido na tela
         <div className="container">
-            <div className="fst-italic mt-3">Inclusão de Editoras</div>
+            <h4 className="fst-italic mt-3">Inclusão de Editoras</h4>
             <form onSubmit={handleSubmit(salvar)}>
                 <div className="form-group">
                     <label htmlFor="nome">Nome:</label>
                     <input type="text" className="form-control" id="nome" required autoFocus {...register("nome")} />
                 </div>
-                <div className="form-group mt-2">
-                    <label htmlFor="rua">Rua:</label>
-                    <input type="text" className="form-control" id="rua" required autoFocus {...register("rua")} />
-                </div>
                 <div className="row mt-2">
+                    <div className="col-sm-8">
+                        <div className="form-group mt-2">
+                            <label htmlFor="rua">Rua:</label>
+                            <input type="text" className="form-control" id="rua" required autoFocus {...register("rua")} />
+                        </div>
+                    </div>
                     <div className="col-sm-4">
                         <div className="form-group mt-2">
                             <label htmlFor="cep">Cep:</label>
                             <input type="text" className="form-control" id="cep" required autoFocus {...register("cep")} />
                         </div>
                     </div>
-                </div>
-                <div className="row mt-2">
-                    <div className="col-sm-4">
-                        <div className="form-group mt-2">
-                            <label htmlFor="cidade">Cidade:</label>
-                            <input type="text" className="form-control" id="cidade" required autoFocus {...register("cidade")} />
+                    <div className="row mt-2">
+                        <div className="col-sm-5">
+                            <div className="form-group mt-2">
+                                <label htmlFor="cidade">Cidade:</label>
+                                <input type="text" className="form-control" id="cidade" required autoFocus {...register("cidade")} />
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row mt-2">
-                    <div className="col-sm-4">
-                        <div className="form-group mt-2">
-                            <label htmlFor="estado">Estado:</label>
-                            <input type="text" className="form-control" id="estado" required autoFocus {...register("estado")} />
+                        <div className="col-sm-4">
+                            <div className="form-group mt-2">
+                                <label htmlFor="estado">Estado:</label>
+                                <input type="text" className="form-control" id="estado" required autoFocus {...register("estado")} />
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="row mt-2">
-                    <div className="col-sm-4">
-                        <div className="col-sm-8">
-                            <div className="form-group">
+                        <div className="col-sm-3">
+                            <div className="form-group mt-2">
                                 <label htmlFor="telefone">Telefone:</label>
                                 <input type="text" className="form-control" id="telefone" required autoFocus {...register("telefone")} />
                             </div>
@@ -70,12 +71,13 @@ const IncluirEditoras = () => {
                     </div>
                 </div>
 
-
                 <input type="submit" className="btn btn-outline-primary mt-3" value="Enviar" />
                 <input type="reset" className="btn btn-outline-danger mt-3 ms-3" value="Limpar" />
 
             </form>
-            <div className="alert"></div>
+            <div className={aviso.startsWith("OK!") ? "alert alert-success" :
+                aviso.startsWith("Erro") ? "alert alert-danger" : ""}> {aviso}
+            </div>
         </div>
 
     );
